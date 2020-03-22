a46ffd4671503bf8c96f6d655cbc37138e614a1c
gmm <m.m.gershevich@ya.ru>
2020-03-22 15:28:05 +0900

    урок 2.1

---
+++ type_var.py
@@ -0,0 +1,24 @@
+#Типы переменных
+
+#Марка автомобилей
+
+name = 'Ford'
+
+print(type(name))
+
+#Возраст
+age=3
+
+print(type(age))
+
+#обьем двигателя
+
+engine_volume=1,6
+
+print(type(engine_volume))
+
+#евть ли люк
+
+sunroof = False
+
+print(type(sunroof))
